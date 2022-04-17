78911a50b753021f4a1e640b280988b46d7198a0
John Godwyn <jon.godwynn@gmail.com>
2022-04-17 12:01:53 +0100

    structured the project

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import SplashScreen from './screens/SplashScreen';
+import Home from './screens/Home';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// all these are used for navigation
+// initialize this for the stack
+const Stack = createNativeStackNavigator();
+// show the header or not
+const screenOptions = {
+  headerShown: false,
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>This is Meel App </Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="SplashScreen"
+        screenOptions={screenOptions}
+      >
+        <Stack.Screen name="SplashScreen" component={SplashScreen} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ config/colors.js
@@ -0,0 +1,10 @@
+export default {
+    // add your colors here
+    black: '#000',
+    white: '#fff',
+    lightGreen: '#3faf',
+    gray: '#E5E5E5',
+    gray2: '#F2F2F2',
+    gray3: '#B2B2B2',
+    orange: '#F8802D',
+};
